83cbbb97a931a45f2b96c74eb767aa072514dc1f
David Peterman <dpeterman@na.hhcpr.hilton.com>
2018-02-18 00:14:44 -0500

    Error, Get and Post done

--- src/server.js
+++ src/server.js
@@ -5,12 +5,41 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
+let posts = [];
+
+let postId = 0;
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
+const handleUserError = (msg, res) => {
+    res.status(STATUS_USER_ERROR).json(msg);
+    return;
+};
 
 // TODO: your code to handle requests
+
+server.get('/posts', (req, res) => {
+  const { term } = req.query;
+  if (term) {
+    const searchedPosts = posts.filter((post) => {
+      return post.title.includes(term) || post.contents.includes(term);
+    });
+    res.json(searchedPosts);
+  } else {
+    res.json(posts);
+  }
+});
+
+server.post('/posts', (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    return handleUserError({ error: 'Must Provide Post Title and Content' }, res);
+  }
+  const post = { id: postId, title, contents };
+  posts.push(post);
+  postId++;
+  res.json(post);
+});
 
 module.exports = { posts, server };
